7450f403eabf83db0a67f4f9f40de7b8237d2305
ThanhMinh <thanhminh339968az@gmail.com>
2022-09-14 21:19:39 +0700

    Add Lesson-08

---
+++ Lesson-08/app.js
@@ -0,0 +1,14 @@
+var Customer = /** @class */ (function () {
+    function Customer(fname, lname) {
+        this.firstName = fname;
+        this.lastName = lname;
+    }
+    return Customer;
+}());
+function customerLogger(customer) {
+    console.log("".concat(customer.firstName, " ").concat(customer.lastName));
+}
+var customer = new Customer('Jane', 'Doe');
+customerLogger(customer);
+//customerLogger('Jane Doe'); //Compiler Error
+//customerLogger(1)

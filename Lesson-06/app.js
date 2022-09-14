48ef5ffe2ca0da710f716b4839cdae3833b7a99b
ThanhMinh <thanhminh339968az@gmail.com>
2022-09-14 20:20:22 +0700

    Add Lesson-06

--- Lesson-06/app.js
+++ Lesson-06/app.js
@@ -7,3 +7,5 @@ var myNumArr = getArray([100, 200, 300]);
 var myStrArr = getArray(['Hello kitti', 'World']);
 console.log(myNumArr);
 console.log(myStrArr);
+// myNumArr.push('Helowr'); complier error
+// myStrArr.push(1);
